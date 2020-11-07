1a95d64cca084ff413f1561fc9408167e474d550
Puneet Gaur <gaur.puneet@outlook.com>
2020-11-07 11:57:53 -0700

    Break test

--- .history/src/test/java/org/springframework/samples/petclinic/model/ValidatorTests_20201107095517.java
+++ .history/src/test/java/org/springframework/samples/petclinic/model/ValidatorTests_20201107095517.java
@@ -45,8 +45,8 @@ class ValidatorTests {
 
 		LocaleContextHolder.setLocale(Locale.ENGLISH);
 		Person person = new Person();
-		person.setFirstName("notempty");
-		person.setLastName("Puneet");
+		person.setFirstName("");
+		person.setLastName("smith");
 
 		Validator validator = createValidator();
 		Set<ConstraintViolation<Person>> constraintViolations = validator.validate(person);
